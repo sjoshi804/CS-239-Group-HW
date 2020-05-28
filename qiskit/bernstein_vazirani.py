7ba55d9345510d51e2e31eab61cdde31e1e7ead9
ohm prakash pagolu <ohm@ohms-MacBook-Pro.local>
2020-05-28 15:22:57 -0700

    Added DJ and BV

---
+++ qiskit/bernstein_vazirani.py
@@ -0,0 +1,151 @@
+from qiskit import QuantumRegister, QuantumCircuit, execute, Aer
+from qiskit.quantum_info.operators import Operator
+from qiskit.visualization import plot_histogram
+
+import numpy as np
+import time
+import random
+
+
+class Solver(object):
+
+    def __init__(self, f, n):   
+        self.f = f
+        self.n = n
+
+        self.__build_circuit()
+
+    def __generate_bit_strings(self, n):
+        '''
+        Input: n
+        Output: A list of bit strings Ex. n=2 -> ["00", "01", "10", "11"]
+
+        A recursive function to generate all possible bit strings with size n.
+        '''
+        if n==1:
+            return ["0", "1"]
+        else:
+            return ["0"+x for x in self.__generate_bit_strings(n-1)]+["1"+x for x in self.__generate_bit_strings(n-1)]
+
+    def __get_tensor(self, bit_string):
+        if bit_string=="0":
+            return np.array([1,0], dtype=np.int16)
+        elif bit_string=='1':
+            return np.array([0,1], dtype=np.int16)
+        else:
+            return np.tensordot(self.__get_tensor(bit_string[0]), self.__get_tensor(bit_string[1:]), 0).ravel().reshape(2**len(bit_string), -1)
+
+    def __modify_bit_string(self, bit_string):
+        res = self.f(bit_string[:-1])
+        if res==0:
+            return bit_string
+        elif res==1:
+            if bit_string[-1]=="0":
+                return bit_string[:-1]+"1"
+            elif bit_string[-1]=="1":
+                return bit_string[:-1]+"0"
+
+    def __produce_u_f_gate(self):
+        bit_strings = self.__generate_bit_strings(self.n+1)
+        xs = list()
+        bs = list()
+        for bit_string in bit_strings:
+            xs.append(self.__get_tensor(bit_string))
+            modified_bit_string = self.__modify_bit_string(bit_string)
+            bs.append(self.__get_tensor(modified_bit_string))
+        
+        X = np.hstack(tuple(xs))
+        B = np.hstack(tuple(bs))
+        
+        A = np.linalg.solve(np.linalg.inv(B), np.linalg.inv(X))
+        self.__Uf = np.array(A)
+        
+    def __build_circuit(self):
+        self.__produce_u_f_gate()
+
+        circuit = QuantumCircuit(self.n + 1, self.n)
+
+        circuit.x(0)
+
+        for i in range(self.n + 1):
+            circuit.h(i)
+
+        circuit.unitary(Operator(self.__Uf), [i for i in range(self.n + 1)], label='Uf')
+
+        for i in range(1, self.n + 1):
+            circuit.h(i)
+
+        circuit.measure([i for i in range(self.n, 0, -1)],[i for i in range(self.n)])
+
+        #print(circuit.draw())
+
+        self.__circuit =circuit
+    
+    def solve(self):
+        '''
+        Run and measure the quantum circuit
+        and return the result.
+        '''
+        simulator = Aer.get_backend("qasm_simulator")
+        job = execute(self.__circuit, simulator, shots=1000)
+
+        # Grab results from the job
+        result = job.result()
+
+        # Returns counts
+        counts = result.get_counts(self.__circuit)
+
+        print(counts)
+
+        for x, y in counts.items():
+            a = x
+            break
+        
+        return a
+
+def random_bit_string_generator(n=1):
+    '''
+    Generates a random bit string of length n
+
+    Input: n (Default: n=1)
+    Output: A bit string Ex. n=7 -> "0101010"
+    '''
+    bit_string = ''
+    for i in range(0,n):
+        bit_string+=str(random.choice([0,1]))
+    return bit_string
+
+def inner_product_mod_2(x,y):
+    '''
+    Calculates inner product from two given bit strings
+    and then applies mod of 2
+
+    Input: x, y (Bit strings)
+    Output: Inner Product mod 2 Ex. x="1011" y="1101" -> (1+0+0+1)%2 = 2%2 = 0.
+    '''
+    # x, y are bit strings
+    sum=0
+    for i,j in zip(x,y):
+        sum+=int(i)*int(j)
+    return sum%2
+
+def addition_mod_2(x,y):
+    '''
+    Adds two bits and then applied mod 2
+
+    Input: x, y (Bits)
+    Output: Sum mod 2 Ex. x=1, y=1 -> (1+1)%2 = 2%2 = 0.
+    '''
+    #x, y are bits which are integers 0 or 1
+    return (x+y)%2
+
+#Test function with a completely randomized value of a and b.
+n = 10
+#random_bit_string_generator(len(x))
+#random.choice([0,1])
+f = lambda x: addition_mod_2(inner_product_mod_2("1010111010", x), 1)
+
+solver = Solver(f, n)
+a = solver.solve()
+print('Value of a: {}'.format(a))
+print('Value of b: {}'.format(f("0"*n)))
---
+++ qiskit/deutsch_jozsa.py
@@ -0,0 +1,153 @@
+from qiskit import QuantumRegister, QuantumCircuit, execute, Aer
+from qiskit.quantum_info.operators import Operator
+from qiskit.visualization import plot_histogram
+
+import numpy as np
+import time
+import random
+
+#Converts integer to bit string of specified length
+def get_bit_string(num, lngt):
+	return bin(num)[2:].zfill(lngt)
+
+def timer(original_function):
+	'''
+	A decorator function to time a function.
+	'''
+	def wrapper_function(*args,**kwargs):
+		start=time.time()
+		result=original_function(*args,**kwargs)
+		stop=time.time()
+		diff=stop-start
+		print('{} took {} seconds\n'.format(original_function.__name__,diff))
+		return result
+	return wrapper_function
+
+class DeutschJozsa:
+
+	def __init__(self, mapping):
+		self.__mapping = mapping
+		self.__num_qubits = int(np.log2(len(mappings)))
+		self.__u_f_dim = 2 ** (self.__num_qubits + 1)
+		self.__Uf = self.__create_Uf()
+		self.__circuit = self.__build_circuit()
+
+	def __create_Uf(self) -> np.ndarray:
+
+		bitsum = sum([int(bit) for bit in self.__mapping.values()])
+
+		#Checking whether the given mapping is constant or balanced
+		if(not(bitsum == 0 or bitsum == 2 ** (self.__num_qubits - 1) or bitsum == 2 ** self.__num_qubits)):
+			raise ValueError("The function must be constant or balanced")
+
+		Uf = np.zeros((self.__u_f_dim , self.__u_f_dim)) #Creating a zero matrix of appropriate dimensions initially
+
+		for i in range(2 ** (self.__num_qubits + 1)): #Going over all bit strings of length num_qubits + 1
+			inp = get_bit_string(i, self.__num_qubits + 1)
+
+			x = inp[0:self.__num_qubits]
+			fx = mappings[x] #fx is the output of f applied on x
+
+			b = inp[self.__num_qubits] #Helper qubit state initially
+
+			if b == fx:
+				bfx = '0' #b^f(x)
+			else:
+				bfx = '1'
+
+			result = x + bfx #This is the resulting qubit states on applying Uf to inp
+
+			row = int(result, 2) #Converting the bitstring to int
+			col = i
+
+			Uf[row][col] = 1 #Generating Uf based on the mapping Uf|x>|b> = |x>|b^f(x)>
+
+		return Uf
+
+	def __build_circuit(self):
+
+		circuit = QuantumCircuit(self.__num_qubits + 1, self.__num_qubits)
+
+		circuit.x(0)
+
+		for i in range(self.__num_qubits + 1):
+			circuit.h(i)
+
+		circuit.unitary(Operator(self.__Uf), [i for i in range(self.__num_qubits + 1)], label='Uf')
+
+		for i in range(1, self.__num_qubits + 1):
+			circuit.h(i)
+
+		circuit.measure([i for i in range(self.__num_qubits, 0, -1)],[i for i in range(self.__num_qubits)])
+		#print(circuit.draw())
+
+		return circuit
+
+	@timer
+	def solve(self):
+		'''
+		Run and measure the quantum circuit
+		and return the result.
+		'''
+		simulator = Aer.get_backend("qasm_simulator")
+		job = execute(self.__circuit, simulator, shots=1000)
+
+		# Grab results from the job
+		result = job.result()
+
+		# Returns counts
+		counts = result.get_counts(self.__circuit)
+		#print("No. of times each state appears:",counts)
+
+		return counts
+
+#Testing
+mappings = {}
+n = int(input("no. of qubits: "))
+print("Type 'y' if you want to give input-output pairs as input or type 'n' : ", end='')
+ir = input()
+if ir == 'y':
+	print("Enter space seperated input, output pairs each in a line:")
+	for i in range(2 ** n):
+		inp, out = input().split()
+		mappings[inp] = out
+else:
+	print("Type 'c' if you want a constant function as input or type 'b' for a random balanced function : ", end='')
+	cb = input()
+	if cb == 'c':
+		const_val = str(random.randint(0,1)) #Setting the output to either bit 0 always or 1 always
+		for i in range(2 ** n):
+			mappings[get_bit_string(i, n)] = const_val
+	else:
+		'''Setting 0's and 1's randomly for each bit string and when 
+		the count of 0 or 1 reaches half, remaining values are set such that it is balanced.'''
+		zero_ct = 0
+		one_ct = 0
+		half = 2 ** (n - 1)
+		for i in range(2 ** n):
+			bit_str = get_bit_string(i, n)
+			if(zero_ct == half):
+				mappings[bit_str] = "1"
+			elif(one_ct == half):
+				mappings[bit_str] = "0"
+			else:
+				rand_val = str(random.randint(0,1))
+				mappings[bit_str] = rand_val
+				if(rand_val == "0"):
+					zero_ct += 1
+				else:
+					one_ct += 1
+
+
+counts = DeutschJozsa(mappings).solve()
+
+zero_state = "0" * n
+
+for state in counts:
+	if(state == zero_state): #Checking if all zeros state occured which means its constant
+		print("Constant")
+		break;
+	else:
+		print("balanced")
+		break;
+
